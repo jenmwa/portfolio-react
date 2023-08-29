4897afd55c6344618a288a3216c27e76d653e333
jenmwa <jenmwa@gmail.com>
2023-08-29 14:51:15 +0200

    .

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,34 +5,29 @@ import { darkModeContext } from "../contexts/darkModeContext"
 import { useReducer } from "react"
 import { DarkModereducer } from "./reducers/DarkModeReducer"
 
-
-
-
-
 export const Layout = () => {
-    const [darkMode, dispatch] = useReducer(DarkModereducer, false);
+  const [darkMode, dispatch] = useReducer(DarkModereducer, false);
 
+  const toggleDarkMode = () => {
+    dispatch({
+      type: 'DARKMODE',
+      payload: false
+    });
+  };
 
-    const toggleDarkMode = () => {
-        dispatch({
-            type: 'DARKMODE',
-            payload: false
-        });
-      };
-   
-    return <>
-    <darkModeContext.Provider value={{ darkMode, toggleDarkMode}}>
-    <div className={darkMode ? 'darkMode' : ''}>
-    <header>
-        <Navigation click={toggleDarkMode}></Navigation>
-    </header>
-    <main>
-        <Outlet></Outlet>
-    </main>
-    <footer>
-    <Footer></Footer>
-    </footer>
-</div>
+  return <>
+    <darkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      <div className={darkMode ? 'darkMode' : ''}>
+        <header>
+          <Navigation click={toggleDarkMode}></Navigation>
+        </header>
+        <main>
+          <Outlet></Outlet>
+        </main>
+        <footer>
+          <Footer></Footer>
+        </footer>
+      </div>
     </darkModeContext.Provider>
-    </>
-  }
+  </>
+}
