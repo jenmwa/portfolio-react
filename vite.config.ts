44a2f7eae5e636e906498f411c62691b30488e33
jenmwa <jenmwa@gmail.com>
2023-08-29 23:11:10 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-react/"
+  base: "/portfolio-react/",
+  define: {
+    'process.env': {},
+  },
 })
 
