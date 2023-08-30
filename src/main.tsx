0bac0fb000949bb5bf5a759f89c486c622cb29ef
jenmwa <jenmwa@gmail.com>
2023-08-30 08:47:25 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.scss'
 import { HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
