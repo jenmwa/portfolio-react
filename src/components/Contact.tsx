ac1225bdccee3294881f130d2e8613cb296c00d7
jenmwa <jenmwa@gmail.com>
2023-09-30 20:09:21 +0200

    mockstructure added for contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,8 +3,25 @@ import { Button } from "./styled/Buttons";
 export const Contact = () => {
   return (
     <>
-      <h2>Contact</h2>
-      <Button>Button in Contact</Button>
+      <div style={{ backgroundColor: "#2a2e45" }}>
+        <h2 style={{ color: "whitesmoke" }}>Contact</h2>
+        <form>
+          <label>
+            Namn:
+            <input type="text"></input>
+          </label>
+          <label>
+            E-post:
+            <input type="mail"></input>
+          </label>
+          <label>
+            <textarea></textarea>
+          </label>
+        </form>
+        <Button style={{ backgroundColor: "whitesmoke", color: "#2a2e45" }}>
+          Button in Contact
+        </Button>
+      </div>
     </>
   );
 };
