b6f434889dd58f17b2fceb9c0dca695df2989145
jenmwa <jenmwa@gmail.com>
2023-10-09 21:37:33 +0200

    button added to test vscode w sonoma

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Section } from "./styled/Sections";
+import { Button } from "./styled/Buttons";
 
 export const Project = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const getBackButton = () => {
+    navigate("/");
+  };
+
   const idToNumber: number = Number(id);
   //rendera lista med projekt och hitta id.
   console.log(idToNumber);
   return (
     <>
       <Section>Projectpage {id} </Section>
+      <Button onClick={getBackButton}>Tillbaka</Button>
     </>
   );
 };
